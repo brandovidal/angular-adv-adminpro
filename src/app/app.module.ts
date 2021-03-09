825dc7c2c89650737f2a1776943128867fc673dd
Brando Vidal <yisusdracon@gmail.com>
2021-03-09 00:12:11 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { NotPageFoundComponent } from './pages/not-page-found/not-page-found.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { BreadcrumbComponent } from './shared/breadcrumb/breadcrumb.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { HeaderComponent } from './shared/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    NotPageFoundComponent,
+    DashboardComponent,
+    BreadcrumbComponent,
+    SidebarComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
