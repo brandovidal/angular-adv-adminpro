98e6aa33da117659f97ce5a03eda3d0253e2a7f8
Brando Vidal <yisusdracon@gmail.com>
2021-03-15 00:52:16 -0500

    Adding custom modules
    Signed-off-by: Brando Vidal <yisusdracon@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,35 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { NotPageFoundComponent } from './pages/not-page-found/not-page-found.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { BreadcrumbComponent } from './shared/breadcrumb/breadcrumb.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Grafica1Component } from './pages/grafica1/grafica1.component';
+
+// Modules
 import { AppRoutingModule } from './app-routing.module';
-import { PagesComponent } from './pages/pages.component';
+import { PagesModule } from './pages/pages.module';
+
+import { NotPageFoundComponent } from './pages/not-page-found/not-page-found.component';
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    RegisterComponent,
+
     NotPageFoundComponent,
-    DashboardComponent,
-    BreadcrumbComponent,
-    SidebarComponent,
-    HeaderComponent,
-    ProgressComponent,
-    Grafica1Component,
-    PagesComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    PagesModule,
+    AuthModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
